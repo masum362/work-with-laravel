436f5644a9e6cf7b025767bf38761b71f0ade2f4
masum362 <masumahmed64077@gmail.com>
2025-02-18 06:01:57 +0600

    route wildcards and view data

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -5,6 +5,20 @@
     <title>Masum Testing | test page</title>
 </head>
 <body>
+    {{-- <p>{{$greetings}}</p>
     this is test page
+    <br>
+    <a href="/">
+    {{
+        $testUsers[0]['username']
+    }}
+    </a>
+    <a href="/">
+    {{
+        $testUsers[1]['username']
+    }}
+    </a> --}}
+
+    <h1>this is the page for test id : {{$id}} and name is: {{$name}}</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,16 @@ Route::get('/', function () {
 
 
 Route::get("/test", function () {
-    return view('test.index');
+
+    $testUsers = [ 
+        ['username' => 'masum362'],
+        ['username' => 'shihab2'],
+    ];
+ 
+    
+    return view('test.index',['greetings'=>'hello world','testUsers'=>$testUsers]);
+});
+
+Route::get('test/{id}/{name}',function($id,$name){
+    return view('test.index',['id'=>$id,'name'=>$name]);
 });
